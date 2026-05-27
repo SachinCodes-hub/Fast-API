5f564d89012b4ab0e05bbd81cb708d402e7bbee3
sachincodes <sachinkarale2005@gmail.com>
2026-05-27 15:29:05 +0530

    pathpara

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from fastapi import FastAPI 
 
 app = FastAPI()
+
+@app.get("/")
+def home():
+    return "Happy fastapi !!!"
+@app.get("/contact")
+def contact():
+    return "contact us details , you can send us anything chat with us here are the contact details of the company"
+
+@app.get("/aboutus")
+def aboutus():
+    return "this is about us page of ours . you ll get all the info about us by reading this page ."
+@app.get("/products")
+def products(): # important function as this will be usefull for mostly all the website like every company has their list of products and all 
+    return "product"
